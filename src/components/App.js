c4c6c7d612339a3d5b075f03fa0e54839dd951e9
izabelka <i.konstanciak@gmail.com>
2018-05-26 20:08:30 -0700

    add api key to .env file, add reset.css file

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import styled from 'styled-components';
+import '../assets/css/reset.css';
 import {
   simpleAction,
   focusOnCreateRoomInput,
